dc98aa13c8a7f36f28d2f8526f3a9397773b6c30
yfm <3032912803@qq.com>
2020-08-25 17:15:20 +0800

    '修正冲突'

--- src/store/reducer/index.js
+++ src/store/reducer/index.js
@@ -1,9 +1,5 @@
 import { combineReducers } from "redux";
 import numReducer from "./numReducer";
-<<<<<<< HEAD
 import loginReducer from './loginReducer'
-export default combineReducers({ numReducer, loginReducer});
-=======
 import ctgReducer from "./ctgReducer"
-export default combineReducers({ numReducer, ctgReducer });
->>>>>>> b9f2b9355c9b81b4eda83858087a9435b7057e07
+export default combineReducers({ numReducer, ctgReducer,loginReducer});
